66c45cdb78f24504370a134ddc4f279af8747982
mcdwayne <1dwayne.mcdaniel@gmail.com>
2018-07-16 12:16:06 -0700

    adding the get tags function to see if this works - likely does not the way I am doing it

--- hello-world/command.php
+++ hello-world/command.php
@@ -9,8 +9,19 @@ if ( ! class_exists( 'WP_CLI' ) ) {
  *
  * @when before_wp_load
  */
-$hello_world_command = function() {
+$hello_world_command = function( $id = 0) {
 	echo "I am a tiny princess \n"; 
+
+     	/**
+     	* Filters the array of tags for the given post.
+     	*
+     	* @since 2.3.0
+     	*
+     	* @see get_the_terms()
+     	*
+     	* @param array $terms An array of tags for the given post.
+     	*/
+    	return apply_filters( 'get_the_tags', get_the_terms( $id, 'post_tag' ) );
 
 	WP_CLI::success( "Hello world." );
 };
